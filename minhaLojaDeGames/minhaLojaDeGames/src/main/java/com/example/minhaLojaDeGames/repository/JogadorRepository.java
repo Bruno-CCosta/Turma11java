3417974703911b75301a81146195dab3cdb9b4e8
Bruno-CCosta <bruuuno.coosta@gmail.com>
2020-11-10 18:50:29 -0300

    Postman

--- minhaLojaDeGames/minhaLojaDeGames/src/main/java/com/example/minhaLojaDeGames/repository/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.minhaLojaDeGames.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.minhaLojaDeGames.model.Jogador;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long>{
-
-}
